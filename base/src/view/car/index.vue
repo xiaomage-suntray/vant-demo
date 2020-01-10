5907b8afd15af6c731b02071c453dc6cee64bb95
suochaochao <suochaochao@suntray.com>
2020-01-10 10:29:36 +0800

    New project

---
+++ base/src/view/car/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <van-collapse v-model="activeNames">
+  <van-collapse-item title="邯郸市今日限行" name="1">  
+    限行尾号 ：<van-tag type="danger">3，8</van-tag>
+    单双号限行 ：<van-tag type="danger">单号限行  </van-tag>
+  </van-collapse-item>
+  <van-collapse-item title="峰峰市区今日限行" name="2">
+    限行尾号 ：<van-tag type="danger">3，8</van-tag>
+    单双号限行 ：<van-tag type="danger">单号限行  </van-tag>
+  </van-collapse-item>
+  <van-collapse-item title="磁县今日限行" name="3">
+    限行尾号 ：<van-tag type="danger">3，8</van-tag>
+    单双号限行 ：<van-tag type="danger">单号限行  </van-tag>
+  </van-collapse-item>
+</van-collapse>
+</template>
+
+<script>
+import Vue from 'vue';
+import { Collapse, CollapseItem,Tag, IndexAnchor  } from 'vant';
+Vue.use(Collapse).use(CollapseItem).use(Tag).use(IndexAnchor);
+export default {
+  data() {
+    return {
+      activeNames: ['1','2','3']
+    };
+  }
+};
+</script>
---
+++ base/src/view/weatherInfo/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <van-collapse v-model="activeNames">
+  <van-collapse-item title="邯郸市天气预报" name="1">  
+    天气 ：<van-tag type="primary">晴</van-tag>
+    温度 ：<van-tag type="primary">晴  </van-tag>
+    风向 ：<van-tag type="primary">东北风  </van-tag>
+    风力 ：<van-tag type="primary">3</van-tag>
+    湿度 ：<van-tag type="primary">68  </van-tag>
+    数据更新时间：<van-tag type="primary">2020-01-08 14:32:39</van-tag>
+  </van-collapse-item>
+  <van-collapse-item title="峰峰市区天气预报" name="2">
+    天气 ：<van-tag type="primary">晴</van-tag>
+    温度 ：<van-tag type="primary">晴  </van-tag>
+    风向 ：<van-tag type="primary">东北风  </van-tag>
+    风力 ：<van-tag type="primary">3</van-tag>
+    湿度 ：<van-tag type="primary">68  </van-tag>
+    数据更新时间：<van-tag type="primary">2020-01-08 14:32:39</van-tag>
+  </van-collapse-item>
+  <van-collapse-item title="磁县天气预报" name="3">
+    天气 ：<van-tag type="primary">晴</van-tag>
+    温度 ：<van-tag type="primary">晴  </van-tag>
+    风向 ：<van-tag type="primary">东北风  </van-tag>
+    风力 ：<van-tag type="primary">3</van-tag>
+    湿度 ：<van-tag type="primary">68  </van-tag>
+    数据更新时间：<van-tag type="primary">2020-01-08 14:32:39</van-tag>
+  </van-collapse-item>
+</van-collapse>
+</template>
+
+<script>
+import Vue from 'vue';
+import { Collapse, CollapseItem,Tag, IndexAnchor  } from 'vant';
+Vue.use(Collapse).use(CollapseItem).use(Tag).use(IndexAnchor);
+export default {
+  data() {
+    return {
+      activeNames: ['1']
+    };
+  }
+};
+</script>
